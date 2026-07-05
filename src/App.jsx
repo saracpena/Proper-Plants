ca2f93709cb7154ff6529863970de6147e8efd4f
Sara <s.cristal.p@gmail.com>
2026-07-05 20:14:47 +0200

    update cart and move functions inside APP

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,37 @@ import { useState } from "react";
 
 export default function App() {
   const [cart, setCart] = useState([]);
+ 
+/** Add Functions **/  
+  function addToCart(plant) {
+    const itemInCart = cart.find((item) => item.id === plant.id);
+  
+    if (itemInCart) {//if card is already in cart, increase quantity by 1
+      setCart(
+        cart.map((item) =>
+          item.id === plant.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        )
+      );
+    } else { //if card is not in cart, add it with quantity of 1
+      setCart([...cart, { ...plant, quantity: 1 }]);
+    }
+  }
+  
+  function updateCartQuantity(plantId, amount) {
+    setCart(
+      cart
+        .map((item) =>
+          item.id === plantId
+            ? { ...item, quantity: item.quantity + amount }
+            : item
+        )
+        .filter((item) => item.quantity > 0)
+    );
+  }
+
+  /** Render **/
   return (
     <div className="app">
       <header>
@@ -14,20 +45,4 @@ export default function App() {
       </div>
     </div>
   );
-}
-
-function addToCart(plant) {
-  const itemInCart = cart.find((item) => item.id === plant.id);
-
-  if (itemInCart) {//if card is already in cart, increase quantity by 1
-    setCart(
-      cart.map((item) =>
-        item.id === plant.id
-          ? { ...item, quantity: item.quantity + 1 }
-          : item
-      )
-    );
-  } else { //if card is not in cart, add it with quantity of 1
-    setCart([...cart, { ...plant, quantity: 1 }]);
-  }
 }
